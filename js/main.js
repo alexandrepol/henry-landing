b0bce838c537d69264b41447a9c10afb7e45a705
alexandrepol <alexandre.pol1996@gmail.com>
2017-02-15 08:18:20 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by POL on 12/02/2017.
+ */
+(function($){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyA-NJEpkQDup6K1HoLiA9-FvkwvE8xCLZc",
+        authDomain: "dm-henry.firebaseapp.com",
+        databaseURL: "https://dm-henry.firebaseio.com",
+        storageBucket: "dm-henry.appspot.com",
+        messagingSenderId: "907133393384"
+    };
+    firebase.initializeApp(config);
+
+
+    //Get Testimonials from Firebase
+
+
+
+})(jQuery)
