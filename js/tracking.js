5ed987fd8b08e4148ab6065c7180554a16d9a60a
IesaAlex <contat@alexandrepol.fr>
2017-02-16 15:57:20 +0100

    Graph

--- js/tracking.js
+++ js/tracking.js
@@ -27,7 +27,7 @@
             let childData = childSnap.val();
             let visitsRef = databaseRef.child('/totalVisitors/');
             visitsRef.on('value', function (snapshot) {
-                let ctaTitle = $('.content-back');
+                let ctaTitle = $('.cta');
                 let ctaLink = $('#cta-link');
 
                 if(ctaLink.length != 0){
@@ -42,6 +42,36 @@
     })
 
     //Show beta-testers
+    let betaRef = databaseRef.child('/beta/');
+    betaRef.on('value', function(snap){
+        let totalUsers = snap.numChildren();
+        let fromFb = 0;
+        let fromGoogle = 0;
+        let fromMail = 0;
+       snap.forEach(function(childSnap){
+          switch (childSnap.val().from){
+              case 'Google':
+                  fromGoogle++;
+                  break;
+              case 'Facebook':
+                  fromFb++;
+                  break;
+              case 'Mail':
+                  fromMail++;
+                  break;
+          }
+       });
+        let statsGoogle = (fromGoogle/totalUsers)*100;
+        let statsMail = (fromMail/totalUsers)*100;
+        let statsFb = (fromFb/totalUsers)*100;
+        createGraph(statsGoogle);
 
+    });
+
+    //Create Graph
+    function createGraph(stats){
+        let graphDiv = $('.graph');
+        graphDiv.append('<div class="stats-item-bar" data-stats ="'+stats+'"></div>');
+    }
 
 })(jQuery)
